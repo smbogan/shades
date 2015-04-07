1191a57a483983c99bb7b7bc69bfb5f15f7ce7a9
Shaun Bogan <smbogan@gmail.com>
2015-04-06 22:04:47 -0500

    Added collections, removed autopublish.

--- shades.js
+++ shades.js
@@ -1,5 +1,66 @@
-Boards = new Mongo.Collection("boards");
 
+var username = "smbogan";
+
+/*
+{
+  _id,
+  [u]sername: "jschmoe",
+  [n]ame: "Joe Schmoe",
+  [p]ermissions: "Aa" (A => administrator, a => API access),
+}
+*/
+Users = new Mongo.Collection("users");
+
+/*
+{
+  _id,
+  [n]ame: "An Organization Name",
+  [t]eams: {
+    _id,
+    [n]ame: "Team Name",
+    [u]sers: [
+      {
+        [u]sername: "jschmoe",
+        [p]ermissions: "" (r => read, w => write )
+      }, {...}, ...
+    ],
+  },
+  [b]oards: {
+    _id,
+    [n]ame: "Board Name",
+    [t]ags: [...]
+  }
+}
+*/
+Organizations = new Mongo.Collection("organizations");
+
+/*
+{
+  [n]ame: "Sprint Name",
+  [s]tate: "p/o/f/c"  (planning, active, finished, closed),
+  [b]oards: [...ids...]
+}
+*/
+Sprints = new Mongo.Collection("sprints");
+
+/*
+{
+  _id,
+  [t]itle: "Store Title",
+  [d]escription: "Description",
+  doneness_[c]riteria: "Doneness Text",
+  ta[g]s: [...],
+  stat[u]s: "u/g/c" (ungroomed, groomed, completed),
+  [p]oints: 45
+  tas[k]s: [
+    {
+      [t]itle: "Task Title",
+      [h]ours: 30,
+    }, ...
+  ]
+}
+*/
+Stories = new Mongo.Collection("stories");
 
 if (Meteor.isClient) {
   // counter starts at 0
@@ -24,9 +85,7 @@ if (Meteor.isClient) {
   });
 
   Template.home.events({
-    'click .gotowallaby': function() {
-      Session.set('page', 'wallaby');
-    }
+
   });
 
   Template.boardslist.helpers({
@@ -38,6 +97,17 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Meteor.publish("Users", function() {
+      var currentUser = Users.findOne({name: username});
+      if(currentUser) {
+        if(currentUser.p.indexOf("A") >= 0) {
+          return Users.fetch({});
+        } else {
+          return Users.find({name: username});
+        }
+      } else {
+        return null;
+      }
+    });
   });
 }
